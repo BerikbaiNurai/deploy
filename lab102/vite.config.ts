2ad6c42cb10a362dab31a446d2019e39b74111e1
Nurai Berikbai <nurajberikbaj@gmail.com>
2026-03-24 18:48:02 +0500

    first commit

---
+++ lab102/vite.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    minify: 'terser',
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules/react') || id.includes('node_modules/react-dom')) {
+            return 'vendor'; // все React и ReactDOM в отдельный чанк
+          }
+        },
+      },
+    },
+  },
+  server: {
+    port: 3000,
+  },
+});
